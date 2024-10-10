120e77b6ed45a3d758bc350c86e48635093b98ba
Akash <akash03xyz@gmail.com>
2024-10-10 13:48:52 +0530

    improved client page

--- adxeto-JavaScript/clients.js
+++ adxeto-JavaScript/clients.js
@@ -1,46 +1,36 @@
-const downArrowEl = document.querySelector('.down-arrow');
-const upArrowEl = document.querySelector('.hide-up-arrow');
-const clientDataLinebreakEl = document.querySelector('.client-data-linebreak');
-const clientDetailsEl = document.querySelector('.flex-client-detail');
+const openDivEl = document.getElementById('open-div');
+const defaultDivEl = document.getElementById('default-div');
+const closeDivEl = document.querySelector('.close-div');
 
-function openClientDetails() {
-  clientDataLinebreakEl.style.display = 'block';
-  clientDetailsEl.style.display = 'flex';
-  downArrowEl.style.display = 'none';
-  upArrowEl.style.display = 'block';
+function openOpenDiv() {
+  openDivEl.style.display = "block";
+  defaultDivEl.style.display = "none";
 }
 
-function closeClientDetails() {
-  clientDataLinebreakEl.style.display = 'none';
-  clientDetailsEl.style.display = 'none';
-  upArrowEl.style.display = 'none';
-  downArrowEl.style.display = 'block';
+function closeOpenDiv() {
+  openDivEl.style.display = "none";
+  defaultDivEl.style.display = "block";
 }
 
-downArrowEl.addEventListener('click', openClientDetails);
-upArrowEl.addEventListener('click', closeClientDetails);
+defaultDivEl.addEventListener('click', openOpenDiv);
+closeDivEl.addEventListener('click', closeOpenDiv);
 
 
 // DirectIQ
 
-const downArrowDirectEl = document.querySelector('.down-arrow-direct');
-const upArrowDirectEl = document.querySelector('.up-arrow-direct');
-const dataLinebreakDirect = document.querySelector('.linebreak-direct');
-const clientDetailDirect = document.querySelector('.client-detail-direct');
+const openDivDirectIQEl = document.getElementById('open-div-directiq');
+const defaultDivDirectIQEl = document.getElementById('default-div-directiq');
+const closeDivDirectIQEl = document.querySelector('.close-div-directiq');
 
-function openDirectClientDetails () {
-  dataLinebreakDirect.style.display = 'block';
-  clientDetailDirect.style.display = 'flex';
-  downArrowDirectEl.style.display = 'none';
-  upArrowDirectEl.style.display = 'block';
+function openOpenDirectIQDiv() {
+  openDivDirectIQEl.style.display = "block";
+  defaultDivDirectIQEl.style.display = "none";
 }
 
-function closeDirectClientDetails() {
-  dataLinebreakDirect.style.display = 'none';
-  clientDetailDirect.style.display = 'none';
-  upArrowDirectEl.style.display = 'none';
-  downArrowDirectEl.style.display = 'block';
+function closeOpenDirectIQDiv() {
+  openDivDirectIQEl.style.display = "none";
+  defaultDivDirectIQEl.style.display = "block";
 }
 
-downArrowDirectEl.addEventListener('click', openDirectClientDetails); 
-upArrowDirectEl.addEventListener('click', closeDirectClientDetails);
+defaultDivDirectIQEl.addEventListener('click', openOpenDirectIQDiv);
+closeDivDirectIQEl.addEventListener('click', closeOpenDirectIQDiv);
