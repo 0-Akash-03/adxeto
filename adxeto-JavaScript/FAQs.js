69b5facef11a543f387957fbaf2399aaecff82de
Akash <akash03xyz@gmail.com>
2024-10-10 16:52:32 +0530

    Improved FAQs Page

--- adxeto-JavaScript/FAQs.js
+++ adxeto-JavaScript/FAQs.js
@@ -1,45 +1,95 @@
-const downSideArrowOneEl = document.querySelector('.d-arrow-one');
-const downSideArrowTwoEl = document.querySelector('.d-arrow-two');
-const downSideArrowThreeEl = document.querySelector('.d-arrow-three');
-const downSideArrowFourEl = document.querySelector('.d-arrow-four');
-const downSideArrowFiveEl = document.querySelector('.d-arrow-five');
-const downSideArrowSixEl = document.querySelector('.d-arrow-six');
+const defaultFirstUlEl = document.querySelector('.default-first-ul');
+const defaultSecondUlEl = document.querySelector('.default-second-ul');
+const defaultThirdUlEl = document.querySelector('.default-third-ul');
+const defaultFourthUlEl = document.querySelector('.default-fourth-ul');
+const defaultFifthUlEl = document.querySelector('.default-fifth-ul');
+const defaultSixthUlEl = document.querySelector('.default-sixth-ul');
 
+const openFirstUlEl = document.querySelector('.open-first-ul');
+const openSecondUlEl = document.querySelector('.open-second-ul');
+const openThirdUlEl = document.querySelector('.open-third-ul');
+const openFourthUlEl = document.querySelector('.open-fourth-ul');
+const openFifthUlEl = document.querySelector('.open-fifth-ul');
+const openSixthUlEl = document.querySelector('.open-sixth-ul');
 
-const upSideArrowOneEl = document.querySelector('.u-arrow-one');
-console.dir(upSideArrowOneEl);
-const upSideArrowTwoEl = document.querySelector('.u-arrow-two');
-const upSideArrowThreeEl = document.querySelector('.u-arrow-three');
-const upSideArrowFourEl = document.querySelector('.u-arrow-four');
-const upSideArrowFiveEl = document.querySelector('.u-arrow-five');
-const upSideArrowSixEl = document.querySelector('.u-arrow-six');
+const closeFirstUlEl = document.querySelector('.close-first-ul');
+const closeSecondUlEl = document.querySelector('.close-second-ul');
+const closeThirdUlEl = document.querySelector('.close-third-ul');
+const closeFourthUlEl = document.querySelector('.close-fourth-ul');
+const closeFifthUlEl = document.querySelector('.close-fifth-ul');
+const closeSixthUlEl = document.querySelector('.close-sixth-ul');
 
-
-function openAnswerText(event) {
-  const answerParagraph = event.target.parentElement.nextElementSibling;
-  answerParagraph.style.display = 'block';
-  event.target.style.display = 'none';
-  event.target.nextElementSibling.style.display = 'block';
+function openFirstAnswer() {
+  defaultFirstUlEl.style.display = 'none';
+  openFirstUlEl.style.display = 'block';
 }
 
-function closeAnswerText(event) {
-  const answerParagraph = event.target.parentElement.nextElementSibling;
-  answerParagraph.style.display = 'none'
-  event.target.style.display = 'none';
-  event.target.parentElement.children[1].style.display = 'block';
+function openSecondAnswer() {
+  defaultSecondUlEl.style.display = 'none';
+  openSecondUlEl.style.display = 'block';
 }
 
-downSideArrowOneEl.addEventListener('click', openAnswerText);
-downSideArrowTwoEl.addEventListener('click', openAnswerText);
-downSideArrowThreeEl.addEventListener('click', openAnswerText);
-downSideArrowFourEl.addEventListener('click', openAnswerText);
-downSideArrowFiveEl.addEventListener('click', openAnswerText);
-downSideArrowSixEl.addEventListener('click', openAnswerText);
+function openThirdAnswer() {
+  defaultThirdUlEl.style.display = 'none';
+  openThirdUlEl.style.display = 'block';
+}
+
+function openFourthAnswer() {
+  defaultFourthUlEl.style.display = 'none';
+  openFourthUlEl.style.display = 'block';
+}
+
+function openFifthAnswer() {
+  defaultFifthUlEl.style.display = 'none';
+  openFifthUlEl.style.display = 'block';
+}
+
+function openSixthAnswer() {
+  defaultSixthUlEl.style.display = 'none';
+  openSixthUlEl.style.display = 'block';
+}
 
 
-upSideArrowOneEl.addEventListener('click', closeAnswerText);
-upSideArrowTwoEl.addEventListener('click', closeAnswerText);
-upSideArrowThreeEl.addEventListener('click', closeAnswerText);
-upSideArrowFourEl.addEventListener('click', closeAnswerText);
-upSideArrowFiveEl.addEventListener('click', closeAnswerText);
-upSideArrowSixEl.addEventListener('click', closeAnswerText);
+function closeFirstAnswer() {
+  defaultFirstUlEl.style.display = 'block';
+  openFirstUlEl.style.display = 'none';
+}
+
+function closeSecondAnswer() {
+  defaultSecondUlEl.style.display = 'block';
+  openSecondUlEl.style.display = 'none';
+}
+
+function closeThirdAnswer() {
+  defaultThirdUlEl.style.display = 'block';
+  openThirdUlEl.style.display = 'none';
+}
+
+function closeFourthAnswer() {
+  defaultFourthUlEl.style.display = 'block';
+  openFourthUlEl.style.display = 'none';
+}
+
+function closeFifthAnswer() {
+  defaultFifthUlEl.style.display = 'block';
+  openFifthUlEl.style.display = 'none';
+}
+
+function closeSixthAnswer() {
+  defaultSixthUlEl.style.display = 'block';
+  openSixthUlEl.style.display = 'none';
+}
+
+defaultFirstUlEl.addEventListener('click', openFirstAnswer);
+defaultSecondUlEl.addEventListener('click', openSecondAnswer);
+defaultThirdUlEl.addEventListener('click', openThirdAnswer);
+defaultFourthUlEl.addEventListener('click', openFourthAnswer);
+defaultFifthUlEl.addEventListener('click', openFifthAnswer);
+defaultSixthUlEl.addEventListener('click', openSixthAnswer);
+
+closeFirstUlEl.addEventListener('click', closeFirstAnswer);
+closeSecondUlEl.addEventListener('click', closeSecondAnswer);
+closeThirdUlEl.addEventListener('click', closeThirdAnswer);
+closeFourthUlEl.addEventListener('click', closeFourthAnswer);
+closeFifthUlEl.addEventListener('click', closeFifthAnswer);
+closeSixthUlEl.addEventListener('click', closeSixthAnswer);
